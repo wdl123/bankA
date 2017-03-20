8438c3b7cf19036ab8c458e1731dbcd236442428
wudongliang <wdl@wudongliangdeMacBook-Pro.local>
2017-03-20 13:58:49 +0800

    Initial Commit

---
+++ bankApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bankApp
+//
+//  Created by wudongliang on 2017/3/20.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bankApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bankApp
+//
+//  Created by wudongliang on 2017/3/20.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
